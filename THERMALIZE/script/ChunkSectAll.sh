481712311ee3e4eb424e64670bac8bfd8d2d5b40
mbaityje <marcoan266bb@hotmail.com>
2018-05-10 14:01:55 -0700

    minor

--- THERMALIZE/script/ChunkSectAll.sh
+++ THERMALIZE/script/ChunkSectAll.sh
@@ -19,9 +19,9 @@ utilDIR=$rootDIR/UTILITIES
 
 #Parameters
 readonly dt=0.0025
-readonly ttot=600000 #`echo 2*10^7|bc` #We will want 10^9 steps
+readonly ttot=`echo 2*10^7|bc` #We will want 10^9 steps
 TLIST="0.49" #"10.0 2.0 0.6 0.466 0.44 0.43 0.42 0.41"
-readonly deltaE=0.00001
+readonly deltaE=0.0001
 
 cd $workDIR
 for T in $(echo $TLIST)
@@ -33,7 +33,7 @@ do
 	for Natoms in 65
 	do
 		cd N$Natoms
-		for ISAM in 1 #$(ls -d S* | sed 's/S//') #ISAM=any sample that has been simulated
+		for ISAM in 0 1 #$(ls -d S* | sed 's/S//') #ISAM=any sample that has been simulated
 		do
 			cd S$ISAM
 			echo ISAM = $ISAM
