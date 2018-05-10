ff100950366bbd90cd82d3388548855d82dd5e57
mbaityje <marcoan266bb@hotmail.com>
2018-05-10 17:03:03 -0400

    minor

--- THERMALIZE/script/ChunkSectAll.sh
+++ THERMALIZE/script/ChunkSectAll.sh
@@ -27,7 +27,7 @@ utilDIR=$rootDIR/UTILITIES
 readonly dt=0.0025
 readonly ttot=`echo 10^9|bc` #We will want 10^9 steps
 TLIST="0.49" #"10.0 2.0 0.6 0.466 0.44 0.43 0.42 0.41"
-readonly deltaE=0.00001
+readonly deltaE=0.0001
 
 cd $workDIR
 for T in $(echo $TLIST)
