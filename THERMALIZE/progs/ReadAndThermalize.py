3a494bce96dbbf6a1b5043fbc5fa4c2419b24e35
mbaityje <marcoan266bb@hotmail.com>
2018-10-04 15:27:41 -0400

    Improve logarithmic trajectory saving in ReadAndThermalize

--- THERMALIZE/progs/ReadAndThermalize.py
+++ THERMALIZE/progs/ReadAndThermalize.py
@@ -27,7 +27,7 @@ from __future__ import print_function #for compatibility with python3.5
 import hoomd #hoomd is the MD package from the Glotzer group
 from hoomd import md #md is the subsection strictly about the properties of the system
 import sys #this has some system tools like sys.exit() that can be useful
-from os import remove #Used to remove the backup file at the end of the run
+from os import remove,mkdir #Used to remove the backup file at the end of the run
 import argparse #for processing arguments
 import numpy as np #Handles some mathematical operations
 from lib import module_potentials as pot
@@ -78,12 +78,13 @@ parser.add_argument('--dt', type=float, required=False, default=0.002, help='dt 
 parser.add_argument('--thermostat', required=False, default='NVE', help='basename of the output files')
 parser.add_argument('--heavyTrajFreq', type=int, required=False, default=0, help='interval between heavy trajectory backups (default:0, means no backups)')
 parser.add_argument('--backupFreq',type=int, required=False, default=0, help='interval between backups (default:0, means no backups)')
-parser.add_argument('--iframe', type=int, required=False, default=0, help='Specify from which frame of the gsd file we want the starting configuration (default:0, the first frame)')
 parser.add_argument('--trajFreq', type=int, required=False, default=0, help='save trajectory every trajFreq steps (default:0, means no trajectory). If negative, use a logarithmic succession of times, where -trajFreq is the number of configurations in the trajectory (or slightly less, since some times two logarithmic times correspond to the same integer time step).')
+parser.add_argument('--iframe', type=int, required=False, default=0, help='Specify from which frame of the gsd file we want the starting configuration (default:0, the first frame)')
 parser.add_argument('-l','--label', required=False, default='thermalized', help='basename for the output files')
 parser.add_argument('--pot_mode', required=False, default='xplor', help='mode for potential (xplor, shift, no_shift)')
 parser.add_argument('--addsteps', action='store_true', help='If activated, nNVTsteps are done from the input configuration. If False, we substract ini_step. [Default: False]')
 parser.add_argument('--startfromzero', action='store_true', help='If activated, the time step is set to zero once the configuration is read. [Default: False]')
+parser.add_argument('--dumpacc', action='store_true', help='If activated, dumps positions, velocities and accelerations in separate .npy files at trajfreq frequency. [Default: False]')
 args = parser.parse_args(more_arguments)
 
 
@@ -100,8 +101,12 @@ print("| dt                 = ",args.dt)
 print("| thermostat         = ",args.thermostat)
 print("| label              = ",args.label)
 print("| pot_mode           = ",args.pot_mode)
+print("| heavyTrajFreq      = ",args.heavyTrajFreq)
+print("| backupFreq         = ",args.backupFreq)
+print("| trajFreq           = ",args.trajFreq)
 print("| startfromzero      = ",args.startfromzero)
 print("| addsteps           = ",args.addsteps)
+print("| dumpacc            = ",args.dumpacc)
 print("------------------------------")
 if args.nNVTsteps<0:
 	raise ValueError('Cannot set a negative number of steps (it is {})'.format(args.nNVTsteps))
@@ -138,7 +143,7 @@ print("------------------------------")
 #
 ################################################################
 NeighborsListLJ = md.nlist.cell()
-print("Setting Kob-Andersen Potential...")
+print("| Setting Kob-Andersen Potential...")
 if Natoms<500:
 	myLjPair=pot.KApotentialShort(NeighborsListLJ, mode=args.pot_mode)
 else:
@@ -149,7 +154,7 @@ else:
 # SET UP ANALYZER
 #
 ################################################################
-print("Setting up Analyzer...",end='\t')
+print("| Setting up Analyzer...",end='\t')
 
 #Name of the log
 logname=args.label+".txt"
@@ -157,7 +162,7 @@ logname=args.label+".txt"
 #These are the observables we want to log
 analyzerManyVariables_quantities = ['temperature', 'pressure', 'potential_energy', 'kinetic_energy', 'momentum'] #, 'volume', 'num_particles']
 
-print("In ",logname," we write ",analyzerManyVariables_quantities)
+print("| In ",logname," we write ",analyzerManyVariables_quantities)
 
 #Every how many integrations we want to log the observables
 analyzer_period=int(5./args.dt) #Take measurements once every 5 Lennard Jones times
@@ -174,9 +179,33 @@ analyzerManyVariables = hoomd.analyze.log(filename=logname, \
 # 
 ################################################################
 
+def SavePosVelAcc():
+	snap=system.take_snapshot()
+	np.save(outPos, np.array(snap.particles.position))
+	np.save(outVel, np.array(snap.particles.velocity))
+	np.save(outAcc, np.array(snap.particles.acceleration))
+	return
+
+if args.dumpacc:
+	namePos='./pos'+args.label+'.npy'
+	nameVel='./vel'+args.label+'.npy'
+	nameAcc='./acc'+args.label+'.npy'
+	outPos=open(namePos,'ab')
+	outVel=open(nameVel,'ab')
+	outAcc=open(nameAcc,'ab')
+
+
 runSteps = max(0,args.nNVTsteps-iniStep) if args.addsteps==False else args.nNVTsteps #If negative, we run no steps
 print("| runSteps = ",runSteps)
 
+
+def SaveGsdNpy(timestep):
+	'''
+	Saves a gsd file with the current state, and npy files with positions(redundant), velocities(redundant) and Accelerations.
+	'''
+	SavePosVelAcc()
+	hoomd.dump.gsd(filename='trajectory'+args.label+'.gsd', overwrite=False, period=None, group=hoomd.group.all(), phase=-1)
+	return
 
 md.integrate.mode_standard(dt=args.dt)
 md.update.zero_momentum(phase=-1)
@@ -189,45 +218,36 @@ if args.trajFreq>0:
 elif args.trajFreq<0:
 	nt=-args.trajFreq
 	it=0
-	listat=tl.ListaLogaritmica(1, runSteps, nt, ints=True, addzero=True)
-	hoomd.dump.gsd(filename='trajectory'+args.label+'.gsd', overwrite=False, period=None, group=hoomd.group.all(),phase=-1)
-	print("listat:", listat)
+	listat=tl.ListaLogaritmica(1, runSteps-1, nt, ints=True, addzero=True)
 	nt=len(listat) #Since it's a logarithmic list of integers, it might end up having less elements than declare
 
-
+	#add a trailing element to the list, to avoid IndexError when the list is reached
+	listat=np.append(listat,[runSteps])
+	if  args.dumpacc:
+		hoomd.analyze.callback(callback=SaveGsdNpy, period = lambda n: listat[n] )
+	else:
+		#If we don't need the accelerations, everything is easy
+		hoomd.dump.gsd(filename='trajectory'+args.label+'.gsd', overwrite=True, period = lambda n: listat[n], group=hoomd.group.all() )
+	print('| nt=',nt)
+	# print("| listat:", listat)
 
 
 
 if args.thermostat == 'NVT' :
-	print(runSteps," NVT steps with the Nose-Hoover thermostat at T=",args.temperature)
+	print("| ",runSteps,"NVT steps with the Nose-Hoover thermostat at T=",args.temperature)
 	integrator = md.integrate.nvt(group=hoomd.group.all(), kT=args.temperature, tau=args.tauT)
-	md.update.zero_momentum(period=10,phase=0)
-	if args.trajFreq>=0:
-		hoomd.run(runSteps, quiet=False)
-	else:
-		for it in range(nt-1):
-			fewSteps=listat[it+1]-listat[it]
-			hoomd.run(fewSteps, quiet=False)
-			hoomd.dump.gsd(filename='trajectory'+args.label+'.gsd', overwrite=False, period=None, group=hoomd.group.all(),phase=-1)
-			it+=1
+	md.update.zero_momentum(period=10./args.dt, phase=0)
+	hoomd.run(runSteps, quiet=False)
 
 elif args.thermostat == 'NVE' :
-	print(runSteps," NVE steps with the NVE thermostat")
+	print("| ",runSteps,"NVE steps with the NVE thermostat")
 	integrator = md.integrate.nve(group=hoomd.group.all())
 	md.update.zero_momentum(period=10,phase=0)
-	if args.trajFreq>=0:
-		hoomd.run(runSteps, quiet=False)
-	else:
-		for it in range(nt-1):
-			fewSteps=listat[it+1]-listat[it]
-			hoomd.run(fewSteps, quiet=False)
-			hoomd.dump.gsd(filename='trajectory'+args.label+'.gsd', overwrite=False, period=None, group=hoomd.group.all(),phase=-1)
-			it+=1
-
+	hoomd.run(runSteps, quiet=False)
 
 elif args.thermostat == 'MB' :
 	if args.trajFreq>=0:
-		print(runSteps," NVT steps with the Andersen thermostat at T=",args.temperature)
+		print("| ",runSteps,"NVT steps with the Andersen thermostat at T=",args.temperature)
 		stepsTauT = int(args.tauT/args.dt)
 		md.update.zero_momentum(period=10,phase=0)
 		integrator = md.integrate.nve(group=hoomd.group.all())
@@ -250,11 +270,16 @@ else:
 ############
 # Finalize #
 ############
+#Disable integrator
 integrator.disable()
+
+#Save final configuration
 finalstatename=args.label+".gsd"
 print("finalstatename=",finalstatename)
 #Write final state
-hoomd.dump.gsd(filename=finalstatename, overwrite=True, truncate=True, period=None, group=hoomd.group.all())
+finalstep=0 if args.thermostat=='MB'else None
+hoomd.dump.gsd(filename=finalstatename, overwrite=True, truncate=True, period=None, group=hoomd.group.all(),time_step=finalstep)
+
 #Remove backup because:
 # -we don't need to waste memory
 # -if the final state is there I need further simulations to start from the final state, not the backup
@@ -263,14 +288,11 @@ try:
 except OSError: #If the file does not exist
 	print("No backup has been generated in this run.")
 	pass
-#Finally, if the simulation was run with MB, i want to set the number of time steps to zero,
-#because with MB I only do the initial thermalization to a very high temperature.
-#Since hoomd does not allow to reset the step number, I need to do a little walkaround.
-if args.thermostat == 'MB':
-	hoomd.context.initialize() #I need to create another simulation context, otherwise this shit bitches when I read_gsd
-	hoomd.init.read_gsd(filename=finalstatename, restart=None, time_step=0)
-hoomd.dump.gsd(filename=finalstatename, overwrite=True, period=None, group=hoomd.group.all())
 
-
+#Close I/O streams
+if args.dumpacc:
+	outPos.close()
+	outVel.close()
+	outAcc.close()
 
 print("Finished!\n\n")
