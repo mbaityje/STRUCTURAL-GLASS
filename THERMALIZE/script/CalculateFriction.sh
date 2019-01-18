6d25aa37d418c3ea80827c9c168852b923cbbf68
mbaityje <marcoan266bb@hotmail.com>
2019-01-18 11:38:04 -0800

    Resolve conflict

--- THERMALIZE/script/CalculateFriction.sh
+++ THERMALIZE/script/CalculateFriction.sh
@@ -21,17 +21,19 @@ echo "T N friction_noise friction_diag" > $frictionFILE
 
 
 declare -A allM
-allM=([5.0]=4 [2.0]=3  [1.0]=3  [0.8]=3  [0.7]=3  [0.6]=3  [0.55]=3  [0.52]=3  [0.5]=3 )
+#allM=([5.0]=4 [2.0]=3  [1.0]=3  [0.8]=3  [0.7]=3  [0.6]=3  [0.55]=3  [0.52]=3  [0.5]=3 ) #For JK
 
-for T in 5.0 2.0 1.0 0.8 0.7 0.6 0.55
+for T in 5.0 2.0 1.0 0.8 0.7 0.6 0.55 0.49 0.47
 do
-	M=${allM[$T]}
+    echo "T = $T"
+    #	M=${allM[$T]} #for JK
+	M=3
 	for N in 1080
 	do
-		fr_diag=$(python $exeDIR/CalculateFriction.py $workDIR/T${T}/N${N}/Cd_NVE.txt --temperature=$T)
-		fr_noise=$(python $exeDIR/CalculateFrictionJK.py $workDIR/T${T}/N${N}/noisecorrJK_NVT_M$M.txt $workDIR/T${T}/N${N}/noisecorrJK_NVT_M$M.npy --temperature=$T)
+		fr_diag=$(python $exeDIR/CalculateFriction.py $workDIR/T${T}/N${N}/Cd_NVT.txt --temperature=$T)
+		fr_noise=$(python $exeDIR/CalculateFriction.py $workDIR/T${T}/N${N}/noisecorr_NVT_combine_M$M.txt --temperature=$T)
+		#fr_noise=$(python $exeDIR/CalculateFrictionJK.py $workDIR/T${T}/N${N}/noisecorrJK_NVT_M$M.txt $workDIR/T${T}/N${N}/noisecorrJK_NVT_M$M.npy --temperature=$T)
 		echo "$T $N $fr_noise $fr_diag" >> $frictionFILE
 	done
 done
 
-python ~/STRUCTURAL-GLASS/THERMALIZE/progs/CalculateFrictionJK.py noisecorrJK_NVT_M4.txt noisecorrJK_NVT_M4.npy -T5.0
