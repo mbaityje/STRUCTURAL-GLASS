4d0b0fa258237743a7f84d286b6041c27387294e
mbaityje <marcoan266bb@hotmail.com>
2019-02-04 19:39:27 -0500

    Upgrade friction calculation

--- THERMALIZE/script/CalculateFriction.sh
+++ THERMALIZE/script/CalculateFriction.sh
@@ -16,6 +16,10 @@ dataDIR=$thermDIR/data
 workDIR=$rootDIR/OUTPUT
 utilDIR=$rootDIR/UTILITIES
 
+LISTAT=${1:-"5.0"}
+LISTAN=${2:-"1080"}
+LISTATHERMOSTAT=${3:-"NVT"}
+
 frictionFILE=$dataDIR/friction.txt
 echo "T N friction_noise friction_diag" > $frictionFILE
 
@@ -23,17 +27,23 @@ echo "T N friction_noise friction_diag" > $frictionFILE
 declare -A allM
 #allM=([5.0]=4 [2.0]=3  [1.0]=3  [0.8]=3  [0.7]=3  [0.6]=3  [0.55]=3  [0.52]=3  [0.5]=3 ) #For JK
 
-for T in 5.0 2.0 1.0 0.8 0.7 0.6 0.55 0.49 0.47
+for T in $(echo $LISTAT)
 do
-    echo "T = $T"
-    #	M=${allM[$T]} #for JK
+	echo "T = $T"
+	#	M=${allM[$T]} #for JK
 	M=3
-	for N in 1080
+	for N in $(echo $LISTAN)
 	do
-		fr_diag=$(python $exeDIR/CalculateFriction.py $workDIR/T${T}/N${N}/Cd_NVT.txt --temperature=$T)
-		fr_noise=$(python $exeDIR/CalculateFriction.py $workDIR/T${T}/N${N}/noisecorr_NVT_combine_M$M.txt --temperature=$T)
-		#fr_noise=$(python $exeDIR/CalculateFrictionJK.py $workDIR/T${T}/N${N}/noisecorrJK_NVT_M$M.txt $workDIR/T${T}/N${N}/noisecorrJK_NVT_M$M.npy --temperature=$T)
-		echo "$T $N $fr_noise $fr_diag" >> $frictionFILE
+		for thermostat in $(echo $LISTATHERMOSTAT)
+		do
+			python $exeDIR/CalculateFrictionNoise.py $workDIR/T${T}/N${N}/noisecorr_NVT_combine_M$M.txt --temperature=$T --thermostat=$thermostat
+			python $exeDIR/CalculateFrictionDiag.py  $workDIR/T${T}/N${N}/Cd_NVT.txt --temperature=$T --thermostat=$thermostat
+
+		# fr_diag=$(python $exeDIR/CalculateFriction.py $workDIR/T${T}/N${N}/Cd_NVT.txt --temperature=$T)
+		# fr_noise=$(python $exeDIR/CalculateFriction.py $workDIR/T${T}/N${N}/noisecorr_NVT_combine_M$M.txt --temperature=$T)
+		# #fr_noise=$(python $exeDIR/CalculateFrictionJK.py $workDIR/T${T}/N${N}/noisecorrJK_NVT_M$M.txt $workDIR/T${T}/N${N}/noisecorrJK_NVT_M$M.npy --temperature=$T)
+		# echo "$T $N $fr_noise $fr_diag" >> $frictionFILE
+		done
 	done
 done
 
